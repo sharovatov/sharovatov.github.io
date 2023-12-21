976de31debd3d3e596f9855e041afb5ea5b70078
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2023-12-21 07:14:35 +0100

    added pandoc conversion for the md files to html and some inital conversions are done

--- mdtohtml.sh
+++ mdtohtml.sh
@@ -1,8 +1,23 @@
 # Directory containing markdown files
-markdown_directory="/e"
+markdown_directory="e" # Replace with your directory path
+
+# Check if the directory exists
+if [ ! -d "$markdown_directory" ]; then
+    echo "Directory not found: $markdown_directory"
+    exit 1
+fi
+
+# Change to the directory
+cd "$markdown_directory"
+
+# Check if there are any markdown files in the directory
+if [ -z "$(ls *.md 2> /dev/null)" ]; then
+    echo "No markdown files found in $markdown_directory"
+    exit 1
+fi
 
 # Loop through all markdown files in the directory
-for file in "$markdown_directory"/*.md
+for file in *.md
 do
     # Define the HTML file name
     html_file="${file%.md}.html"
@@ -10,3 +25,5 @@ do
     # Convert markdown to HTML
     pandoc "$file" -o "$html_file"
 done
+
+
